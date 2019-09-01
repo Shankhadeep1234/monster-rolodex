ca5f3a4df3ef121be8bfc14b5cca5cb20a4346cb
Shankhadeep <shankhadeepraj@gmail.com>
2019-09-01 11:43:54 +0530

    Updates

---
+++ precache-manifest.0049ae733e6d857278248223e459e1a4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5125040e51dd1b720e0fd5a8f22490e7",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "d195a244f5eebd7c557d",
+    "url": "/monster-rolodex/static/css/main.b4546e2d.chunk.css"
+  },
+  {
+    "revision": "ebdf587097504289c06e",
+    "url": "/monster-rolodex/static/js/2.3adefb50.chunk.js"
+  },
+  {
+    "revision": "d195a244f5eebd7c557d",
+    "url": "/monster-rolodex/static/js/main.bce6dd50.chunk.js"
+  },
+  {
+    "revision": "b9d608cb1517ab0e40b5",
+    "url": "/monster-rolodex/static/js/runtime~main.8833da29.js"
+  }
+]);
